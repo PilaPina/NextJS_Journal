62b0dd1c343ecbef2e3ca9e99512beddaa7289ef
PilaPina <solald@gmail.com>
2024-01-15 13:55:06 +0000

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
+
+/*
+In Next.js, you can use a special layout.tsx file to create UI that is shared between multiple pages
+
+By adding Inter to the <body> element above, the font will be applied throughout your application. 
+Here, you're also adding the Tailwind antialiased class which smooths out the font. 
+It's not necessary to use this class, but it adds a nice touch.
+
+This is the root layout and is required 
+*/
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,14 @@
+"use client"
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+//added the import link, i'm about to change the way we navigate by replacing <a> in code below to link. Then there is not a reload for the entire page. 
+import { usePathname } from 'next/navigation';  //this checks the path and implements the common UI pattern to show an active link by indicating to the user what page they are currently on
+import clsx from 'clsx';  //this is so I can conditionally apply class names to the links
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,19 +23,26 @@ const links = [
 ];
 
 export default function NavLinks() {
+
+  const pathname = usePathname();  //assigning the path to a variable called pathname
+ 
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}>
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
