e1917950078bb726bc735a86c9422c9b88f79164
PilaPina <solald@gmail.com>
2024-01-17 20:25:38 +0000

    finished tutorial but with errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title: {   //The %s in the template will be replaced with the specific page title.
+    template: '%s | Acme Dashboard', // you can use the title.template field in the metadata object to define a template for your page titles. This template can include the page title, and any other information you want to include.
+    default: 'Acme Dashboard',        
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
